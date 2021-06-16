8f7f32e41b358ab5f265d8dc2da7a62b2768d10c
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2021-06-16 20:50:49 +0100

    Basic Terraform

---
+++ setup.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2" //London
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "bucket-name"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
